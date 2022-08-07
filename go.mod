e02f7ecf46cb6e22da5785e90ae83264c6c0d73f
Shivansh Kuchchal <shivanshbox@gmail.com>
2022-08-07 16:27:59 +0530

    feat(core): add api router and configs file

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/shivanshkc/ledgerguard
 go 1.17
 
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/spf13/viper v1.12.0
 	go.uber.org/zap v1.21.0
 )
--- main.go
+++ main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/shivanshkc/ledgerguard/src/configs"
+	"github.com/shivanshkc/ledgerguard/src/logger"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Prerequisites.
+	ctx, conf, log := context.Background(), configs.Get(), logger.Get()
+
+	// Logging the HTTP server details.
+	log.Info(ctx, &logger.Entry{Payload: fmt.Sprintf("%s v%s http server starting at: %s",
+		conf.Application.Name, conf.Application.Version, conf.HTTPServer.Addr)})
+
+	// Starting the HTTP server.
+	if err := http.ListenAndServe(conf.HTTPServer.Addr, handler()); err != nil {
+		panic("failed to start http server:" + err.Error())
+	}
+}
+
+// handler is responsible to handle all incoming HTTP traffic.
+func handler() http.Handler {
+	router := mux.NewRouter()
+
+	// Client-facing authentication API.
+	router.HandleFunc("/api/auth/{provider_id}", nil).Methods(http.MethodOptions, http.MethodGet)
+	// SSO-provider-facing callback API.
+	router.HandleFunc("/api/auth/{provider_id}/callback", nil).Methods(http.MethodOptions, http.MethodGet)
+
+	// Get user API.
+	router.HandleFunc("/api/user", nil).Methods(http.MethodOptions, http.MethodGet)
+	// Verify user API.
+	router.HandleFunc("/api/user", nil).Methods(http.MethodOptions, http.MethodHead)
+
+	return router
+}
